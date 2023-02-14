b0c847a854cc89e9614c576db6e4b7431511daf4
Leestan360 <stanleymayore2@gmail.com>
2023-02-14 12:01:59 +0300

    Create the dashboard structure

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,78 +1,171 @@
 import React from "react";
+import ReceiptIcon from "@mui/icons-material/Receipt";
 
 function Dashboard() {
   return (
-    <div className="flex w-screen ml-[280px] bg-red-300">
-      <div className="w ">
-        <div>
+    <div className="flex w-full h-screen">
+      <div className="w-3/5">
+        <div className="border-2 border-blue-500 w-[96%] mx-auto my-4 rounded-lg bg-white shadow-xl">
           <p>Sales Overview</p>
-          <div>
+          <div className="flex justify-evenly">
             <div>
               <p>Sales</p>
-              <p>Icon</p>
-              <p>Money</p>
+              <ReceiptIcon />
+              <p>Ksh 1000</p>
             </div>
             <div>
-              <p>Sales</p>
-              <p>Icon</p>
-              <p>Money</p>
+              <p>Revenue</p>
+              <ReceiptIcon />
+              <p>Ksh 20,000</p>
             </div>
             <div>
-              <p>Sales</p>
-              <p>Icon</p>
-              <p>Money</p>
+              <p>Profit</p>
+              <ReceiptIcon />
+              <p>Ksh 2000</p>
             </div>
             <div>
-              <p>Sales</p>
-              <p>Icon</p>
-              <p>Money</p>
+              <p>Cost</p>
+              <ReceiptIcon />
+              <p>Ksh 2333</p>
             </div>
           </div>
         </div>
-        <div>
+        <div className="border-2 border-blue-500 w-[96%] mx-auto my-4 rounded-lg bg-white shadow-xl">
           <p>Sales Overview</p>
-          <div>
+          <div className="flex justify-evenly">
             <div>
               <p>Sales</p>
-              <p>Icon</p>
-              <p>Money</p>
+              <ReceiptIcon />
+              <p>Ksh 1000</p>
             </div>
             <div>
-              <p>Sales</p>
-              <p>Icon</p>
-              <p>Money</p>
+              <p>Revenue</p>
+              <ReceiptIcon />
+              <p>Ksh 20,000</p>
             </div>
             <div>
-              <p>Sales</p>
-              <p>Icon</p>
-              <p>Money</p>
+              <p>Profit</p>
+              <ReceiptIcon />
+              <p>Ksh 2000</p>
             </div>
             <div>
-              <p>Sales</p>
-              <p>Icon</p>
-              <p>Money</p>
+              <p>Cost</p>
+              <ReceiptIcon />
+              <p>Ksh 2333</p>
             </div>
           </div>
         </div>
-        <div>
-          <p>graph</p>
+        <div className="border-2 border-blue-500 w-[96%] mx-auto my-4 rounded-lg bg-white shadow-xl">
+          <p>Sales Overview</p>
+          <div className="flex justify-evenly">
+            <div>
+              <p>Sales</p>
+              <ReceiptIcon />
+              <p>Ksh 1000</p>
+            </div>
+            <div>
+              <p>Revenue</p>
+              <ReceiptIcon />
+              <p>Ksh 20,000</p>
+            </div>
+            <div>
+              <p>Profit</p>
+              <ReceiptIcon />
+              <p>Ksh 2000</p>
+            </div>
+            <div>
+              <p>Cost</p>
+              <ReceiptIcon />
+              <p>Ksh 2333</p>
+            </div>
+          </div>
         </div>
-        <div>
-          <p>top seling</p>
+        <div className="border-2 border-blue-500 w-[96%] mx-auto my-4 rounded-lg bg-white shadow-xl">
+          <p>Sales Overview</p>
+          <div className="flex justify-evenly">
+            <div>
+              <p>Sales</p>
+              <ReceiptIcon />
+              <p>Ksh 1000</p>
+            </div>
+            <div>
+              <p>Revenue</p>
+              <ReceiptIcon />
+              <p>Ksh 20,000</p>
+            </div>
+            <div>
+              <p>Profit</p>
+              <ReceiptIcon />
+              <p>Ksh 2000</p>
+            </div>
+            <div>
+              <p>Cost</p>
+              <ReceiptIcon />
+              <p>Ksh 2333</p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="bg-black">
-        <div className="">
-          <p>fishfhdo</p>
+      <div className="w-2/5">
+        <div className="border-2 border-blue-500  mr-5 my-4 rounded-lg bg-white shadow-xl">
+          <p>Sales Overview</p>
+          <div className="flex justify-evenly">
+            <div>
+              <p>Sales</p>
+              <ReceiptIcon />
+              <p>Ksh 1000</p>
+            </div>
+            <div>
+              <p>Revenue</p>
+              <ReceiptIcon />
+              <p>Ksh 20,000</p>
+            </div>
+          </div>
         </div>
-        <div>
-          <p>fishfhdo</p>
+        <div className="border-2 border-blue-500  mr-5 my-4 rounded-lg bg-white shadow-xl">
+          <p>Sales Overview</p>
+          <div className="flex justify-evenly">
+            <div>
+              <p>Sales</p>
+              <ReceiptIcon />
+              <p>Ksh 1000</p>
+            </div>
+            <div>
+              <p>Revenue</p>
+              <ReceiptIcon />
+              <p>Ksh 20,000</p>
+            </div>
+          </div>
         </div>
-        <div>
-          <p>fishfhdo</p>
+        <div className="border-2 border-blue-500  mr-5 my-4 rounded-lg bg-white shadow-xl">
+          <p>Sales Overview</p>
+          <div className="flex justify-evenly">
+            <div>
+              <p>Sales</p>
+              <ReceiptIcon />
+              <p>Ksh 1000</p>
+            </div>
+            <div>
+              <p>Revenue</p>
+              <ReceiptIcon />
+              <p>Ksh 20,000</p>
+            </div>
+          </div>
         </div>
-        <div>
-          <p>fishfhdo</p>
+        <div className="border-2 border-blue-500  mr-5 my-4 rounded-lg bg-white shadow-xl">
+          <p>Sales Overview</p>
+          <div className="flex justify-evenly">
+            <div>
+              <p>Sales</p>
+              <ReceiptIcon />
+              <p>Ksh 1000</p>
+            </div>
+            <div>
+              <p>Revenue</p>
+              <ReceiptIcon />
+              <p>Ksh 20,000</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
